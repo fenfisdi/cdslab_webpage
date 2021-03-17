cdcf34e5d1f0d8cdad08771ce91cf52a9dc90035
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-03-17 15:57:45 -0500

    create dev branch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Suspense } from 'react'
-import Layout from './components/layouts/Layout'
-import { useStore } from './store/storeContext'
-import { GlobalStyles } from './styles/GlobalStyles'
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-import './styles/styles.css'
-import { LoginPage } from './pages/LoginPage'
-import { NotFoundPage } from './pages/NotFoundPage'
+import React, { Suspense } from "react";
+import Layout from "./components/layouts/Layout";
+import { useStore } from "./store/storeContext";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import "./styles/styles.css";
+import { LoginPage } from "./pages/LoginPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
-import Dashboard from './components/layouts/Dashboard'
-import ErrorBoundary from './components/ErrorBoundary'
-import { MiniLoader } from './components/layouts/MiniLoader'
-import RegisterPage from './pages/RegisterPage'
+import Dashboard from "./components/layouts/Dashboard";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { MiniLoader } from "./components/layouts/MiniLoader";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
   const {
     state: {
-      session: { isAuth }
-    }
-  } = useStore()
+      session: { isAuth },
+    },
+  } = useStore();
 
-  const SimulationsPage = React.lazy(() => import('./pages/Simulations'))
+  const SimulationsPage = React.lazy(() => import("./pages/Simulations"));
 
   // A wrapper for <Route> that redirects to the login
   // screen if you're not yet authenticated.
+  // Dev enviroment
   const PrivateRoute = ({ component, ...rest }) => {
     return (
       <Route
@@ -33,32 +39,36 @@ const App = () => {
           ) : (
             <Redirect
               to={{
-                pathname: '/',
-                state: { from: location }
+                pathname: "/",
+                state: { from: location },
               }}
             />
-          )}
+          )
+        }
       />
-    )
-  }
+    );
+  };
 
   return (
     <ErrorBoundary>
-      <Suspense fallback={<MiniLoader/>}>
+      <Suspense fallback={<MiniLoader />}>
         <Router>
-          <GlobalStyles/>
+          <GlobalStyles />
           <Layout>
             <Switch>
-              <Route exact path='/' component={LoginPage}/>
-              <PrivateRoute path='/simulations' component={<SimulationsPage/>}/>
-              <Route exact path='/register' component={RegisterPage}/>
-              <Route component={NotFoundPage}/>
+              <Route exact path="/" component={LoginPage} />
+              <PrivateRoute
+                path="/simulations"
+                component={<SimulationsPage />}
+              />
+              <Route exact path="/register" component={RegisterPage} />
+              <Route component={NotFoundPage} />
             </Switch>
           </Layout>
         </Router>
       </Suspense>
     </ErrorBoundary>
-  )
-}
+  );
+};
 
-export default App
+export default App;
