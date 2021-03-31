3fa7ddda95526b3b3243549e13a95a00ac9d00e3
Alejandro Campillo <alucardcampillo@gmail.com>
2021-03-31 14:59:53 -0500

    fix: remove unnecessary elements

--- src/pages/QRPage/state.js
+++ src/pages/QRPage/state.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useUserActions } from '../../actions/userActions'
 import { useStore } from '../../store/storeContext'
 
