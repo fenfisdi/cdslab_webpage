5da2e2f282e390881ed99e817f068f4854a8b0f2
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-05-04 10:17:15 -0500

    fix: changing auth variable on app.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
       <Route
         {...rest}
         render={({ location }) =>
-          allow ? (
+          isAuth ? (
             <Dashboard> {component} </Dashboard>
           ) : (
             <Redirect
