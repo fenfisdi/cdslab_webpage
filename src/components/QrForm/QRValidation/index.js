f220b21d38774a9c2ed2ab59fd4b031a82e82673
Alejandro Campillo <alucardcampillo@gmail.com>
2021-05-07 14:16:12 -0500

    feat: translate QR Validation component

--- src/components/QrForm/QRValidation/index.js
+++ src/components/QrForm/QRValidation/index.js
@@ -1,17 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import { Input } from '../../ui/Input'
 import { useInputValue } from '../../ui/Input/useInputValue'
 import { VALIDATORS_QR_VALUE } from  './validators'
 import LoaderComponent from '../../ui/Loader'
+import { languageContext } from '../../../config/languageContext'
 
 const QRvalidation = ({ eventEmitter,loading,picture}) => {
   
+  const { t } =  useContext(languageContext)
   const otp_code = useInputValue('', VALIDATORS_QR_VALUE.otp_code, {
     name: 'otp_code',
     type: 'int',
-    label: 'Code',
+    label: t('qrForm.code'),
   })
 
   const handleClick = () => {
@@ -24,7 +26,7 @@ const QRvalidation = ({ eventEmitter,loading,picture}) => {
       {loading && <LoaderComponent width="100%" height={80} marginTop="20px"/>}
       {!loading && <Fragment>
         <Grid item xs={12} style={{ marginTop: '2%' }}>
-          <h4>Please enter verification code.</h4>
+          <h4>{t('qrForm.enterCode')}</h4>
         </Grid>
         <Grid item xs={picture ? 2:12}>
           <Input
@@ -45,7 +47,7 @@ const QRvalidation = ({ eventEmitter,loading,picture}) => {
             className={{}}
             disabled={false}
           >
-            Continue
+            {t('qrForm.continue')}
           </Button>
         </Grid>
       </Fragment>}
