b2d767fb91356cc1d2833643f7f3afcbf7b11e91
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-05-12 17:17:04 -0500

    feat: add representation greek capital letter

--- src/components/ui/Title/index.js
+++ src/components/ui/Title/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
 
 export const TitleComponent = ({ justify, alignItems, title, variant, xs, unit='' }) => {
+  const parser = new DOMParser()
+  const decodedString = parser.parseFromString(`<!doctype html><body>${unit}`, 'text/html').body.textContent
   return (
     <Grid item container justify={justify} alignItems={alignItems} xs={xs || 12} spacing={1}>
-      <Typography variant={variant}>{`${title}   `}</Typography>
-      <Typography variant={'span'} style={{'marginLeft':'5px'}}>{unit}</Typography>
+      <Typography variant={variant}>{`${title}`}</Typography>
+      <Typography variant={'span'} style={{'marginLeft':'5px'}}>{decodedString}</Typography>
     </Grid>
   )
 }
