f17c5a27fd95596f63c3374397d0198e468724d5
Alejandro Campillo <alucardcampillo@gmail.com>
2021-05-17 22:17:03 -0500

    feat: create build save button table component

--- src/pages/UserManagementPage/index.js
+++ src/pages/UserManagementPage/index.js
@@ -1,84 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
-import SettingsIcon from '@material-ui/icons/Settings'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import {TableComponent} from '../../components/UserManagement/ShowTableComponent/index'
+import React, { Suspense } from 'react'
+import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import { UserManagementPageContainer } from './styles'
+import FullWidthTabs from '../../components/Taps'
+import LoaderComponent from '../../components/ui/Loader'
 
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props
+const UserManagementPage = () => {
+  const match = useRouteMatch()
+  const UserManagementMainPage = React.lazy(() => import('./UserManagementMainPage'))
+  
+  const tabs = [
+    {
+      label: 'Users Management',
+      path:  match.path,
+      disabled : false,
+      icon : 'cmodels_SVG'
+    },
+    {
+      label: 'Sys Management',
+      path: `${match.path}/newSimulations`,
+      disabled : true,
+      icon: 'agents_SVG'
+    },
+  ]
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-force-tabpanel-${index}`}
-      aria-labelledby={`scrollable-force-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+    <>
+      <FullWidthTabs tabs={tabs} />
+      
+      <UserManagementPageContainer>
+        <Suspense fallback={<LoaderComponent width={50} height={50} marginTop={5}/>}>
+          <Switch>
+            <Route path={match.path} exact component={UserManagementMainPage} />
+          </Switch>
+        </Suspense>
+      </UserManagementPageContainer>
+    </>
   )
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-}))
-
-export default function UserManagementPage() {
-  const isRoot = true //tag del back
-  const classes = useStyles()
-  const [value, setValue] = React.useState(0)
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="fullWidth"
-          scrollButtons="on"
-          indicatorColor="primary"
-          textColor="primary"
-          aria-label="icon tabs example"
-        >
-          <Tab label="Users Management" icon={<SupervisorAccountIcon fontSize="large"/>}/>
-          <Tab label="Sys Management" icon={<SettingsIcon  fontSize="large"/>}/>
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <TableComponent/>
-        {isRoot ? <TableComponent adminTable={isRoot}/> : <></>}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-    </div>
-  )
-}
-
-
+export default UserManagementPage
---
+++ src/pages/UserManagementPage/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const UserManagementPageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 70px;
+`
