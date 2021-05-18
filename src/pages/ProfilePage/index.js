6346bac736f58ad29e28ef93cb8f19afe2ad921f
Alejandro Campillo <alucardcampillo@gmail.com>
2021-05-18 18:07:19 -0500

    build: links to setup the profile settings

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import Link from '@material-ui/core/Link'
+import {useProfilePageStyles } from './styles'
+
 
 const ProfilePage=()=>{
+  const classes = useProfilePageStyles()
 
   return(
-    <h2>Hola a todos</h2>
+    <Grid container  spacing={8} className={classes.root} direction="column" justify="center"  alignItems="center">
+      <Grid item>
+        <AccountCircleIcon style={{ fontSize: 80, color: '#827C02'}} />
+      </Grid>
+      <Grid item>
+        <Link href="#" onClick={console.log(1)} color="inherit">
+          Update personal data
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link href="#" onClick={console.log(1)} color="inherit">
+          Change password
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link href="#" onClick={console.log(1)} color="inherit">
+          Change QR bindings
+        </Link>
+      </Grid>
+      <Grid container  direction='column' justify='center' alignItems='flex-end'>
+        <Grid item className={classes.item}>
+          <h4>Notify me when a simulation finishes</h4>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
---
+++ src/pages/ProfilePage/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useProfilePageStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '5%',
+    marginRight: '10%'
+  },
+  item: {
+    marginRight: '10%'
+  },
+}))
