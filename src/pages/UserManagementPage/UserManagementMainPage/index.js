e54267b49048f982ee141c57827d201f88b2d34c
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-05-27 15:16:24 -0500

    fix:delete unused variables

--- src/pages/UserManagementPage/UserManagementMainPage/index.js
+++ src/pages/UserManagementPage/UserManagementMainPage/index.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import {TableComponent} from '../../../components/UserManagement/ShowTableComponent/index'
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
-import SaveIcon from '@material-ui/icons/Save'
 import FullWidthTabs from '../../../components/Taps'
 import { UserManagementPageMainContainer } from './styles'
 import CompartmentalButton from '../../../components/CompartmentalModels/CompartmentalButton'
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}))
 const UserManagementMainPage = () => {
 
-  
-
-  const classes = useStyles()
   const isRoot = true
   
   const tabs = [
