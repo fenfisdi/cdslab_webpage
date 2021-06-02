79cb11bd87441b8f0e1bc2c17769cc8725740b27
Andres Herrera <andresherrerab97@gmail.com>
2021-06-02 11:38:06 -0500

    feat: create page agents based models

---
+++ src/constants/agents.js
@@ -0,0 +1,19 @@
+import SvgNote from '../assets/icons/SvgNote'
+import SvgPlus from '../assets/icons/SvgPlus'
+
+export const OPTIONS_AGENTS_MAIN = [
+  {
+    titleIcon: SvgPlus,  
+    name: 'New configuration',
+    indetifier: 'new_simulation',
+    url: 'agentsModels/newConfiguration',
+    ruta: 'newConfiguration'
+  },
+  {
+    titleIcon:SvgNote,
+    name: 'My simulations',
+    indetifier: 'my_simulations',
+    url: 'agentsModels/mySimulations',
+    ruta: 'mySimulations'
+  }
+]
---
+++ src/pages/AgentsBasedModelPage/AgentsMainPage/styles.js
@@ -0,0 +1,40 @@
+import { makeStyles } from '@material-ui/core/styles'
+import styled from 'styled-components'
+
+export const useAgentsMainPageStyles = makeStyles((theme) => ({
+  formBody: {
+    marginTop: '20px',
+    marginBottom: '20px',
+    padding: theme.spacing(2),
+    'box-shadow': '-1px 0px 8px 5px rgba(0,0,0,0.14)',
+    background: '#ECEFF1',
+    ...theme.font.paragraphs,
+    height: '100%',
+    overflow: 'hidden',
+    width: '100%',
+    color: '#000504'
+  },
+  loading: {
+    'min-height': ' 500px',
+    'align-items': 'center'
+  }
+}))
+
+
+export const AgentslMainPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 3%;
+`
+
+export const AgentsMainPageCards = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top:30px
+`
