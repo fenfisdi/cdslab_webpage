2f25afbc0c50ac754670b88b552cb39fa6842ad6
danielCendales0821 <danielcendales0821@gmail.com>
2021-06-02 22:28:08 -0500

    fix: implement correction styles profile - sys management - user management

---
+++ src/pages/UserManagementPage/UserManagementMainPage/constants.js
@@ -0,0 +1,4 @@
+export const userManagementMessage = {
+  title: 'User status modification: ',
+  message: 'If the status of an user gets toggled from Active to Non-Active, all stored data for the given user gets scheduled for removal'
+}
