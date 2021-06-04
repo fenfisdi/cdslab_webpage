8034c2df63e4930a9c68a7da246d687be3f5d0a8
jrodriguezcIntergrupo <jrodriguezc@intergrupo.com>
2021-06-04 15:15:16 -0500

    style: implement functionality style

--- src/pages/AgentsBasedModelPage/AgentsMobilityGroups/state.js
+++ src/pages/AgentsBasedModelPage/AgentsMobilityGroups/state.js
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
 import { useHistory, useRouteMatch } from 'react-router'
 
 export const useAgentsMobilityGroups = () => {
   const history = useHistory()
   const match = useRouteMatch()
+  const [mobility, setmobility] = useState([])
+  const [weight, setWeight] = useState({
+    bandwidth: '',
+    algorithm: '',
+    kernel: '',
+    metric: '',
+    atol: '',
+    rtol: '',
+    breadthFirst: '',
+    leafSize: '',
+    metricParams: ''
+  })
+  const [numpy, setNumpy] = useState({
+    parameter: ''
+  })
+  const [empirical, setEmpirical] = useState({
+    bandwidth: '',
+    algorithm: '',
+    kernel: '',
+    metric: '',
+    atol: '',
+    rtol: '',
+    breadthFirst: '',
+    leafSize: '',
+    metricParams: ''
+  })
+  const mobilityArray = []
 
-  const handleDeleteItem = () => {
-
+  const handleDeleteItem = (index) => {
+    mobilityArray.splice(1, index)
   }
 
   const handleCheckItem = () => {
@@ -13,11 +41,24 @@ export const useAgentsMobilityGroups = () => {
   }
 
   const handleConfigItem = () => {
-    
+    //open modal
+     
   }
 
+  useEffect(() => {
+    setmobility(mobilityArray)
+  }, [mobilityArray])
+
   const handleAddItem = () => {
-    
+    mobilityArray.push({
+      name: '',
+      id: '',
+      distributionType: '',
+      constant: '',
+      weight: weight,
+      numpy: numpy,
+      empirical: empirical
+    })
   }
 
   const redirectToSusceptibilityGroupsPage = () => {
