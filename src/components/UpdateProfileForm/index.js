b4cdc63b934a6456f4ca8f322856efefcf797dd9
Alejandro Campillo <alucardcampillo@gmail.com>
2021-06-13 22:55:59 -0500

    feat: updated form

--- src/components/UpdateProfileForm/index.js
+++ src/components/UpdateProfileForm/index.js
@@ -1,41 +1,103 @@
 import { Grid, Paper } from '@material-ui/core'
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState, useContext } from 'react'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
-import { PhoneNumber } from '../ui/PhoneNumber'
-import theme from '../../styles/cdslabTheme'
 import { Input } from '../ui/Input'
-import { useUpdateProfileFormStyles } from './styles'
-import {useRegisterFormState} from '../Register/RegisterForm/state'
 import { TitleComponent } from '../ui/Title'
+import theme from '../../styles/cdslabTheme'
+import { useUpdateProfileFormStyles } from './styles'
+import { PhoneNumber } from '../ui/PhoneNumber'
+import { PasswordChecker } from '../Register/PasswordChecker'
 import Button from '@material-ui/core/Button'
+import { userUpdateFormState } from './state'
+import LoaderComponent from '../ui/Loader'
+import { languageContext } from '../../config/languageContext'
 
-const UpdateProfileForm = () => {
+const UpdateProfileForm = (eventEmitter, loading) => {
 
-  const fieldsData = useRegisterFormState()
   const classes = useUpdateProfileFormStyles(theme)
+  const [isValid, setIsvalid] = useState(false)
+  const [verificationPassword, setVerificationPassword] = useState(false)
+  const fieldsData = userUpdateFormState()
   const [phonePrefix, setPrefix] = useState('57')
+  const { t } = useContext(languageContext)
 
   const {
+    name,
+    lastName,
     email,
+    dateBirth,
     phoneNumber,
     institution,
     institutionAffiliation,
     profession,
+    password,
+    securityQuestion1,
+    securityQuestion2,
+    securityAnswer1,
+    securityAnswer2
   } = fieldsData
+
+  useEffect(() => {
+    let notIsValid = false
+    for (var key in fieldsData) {
+      if (
+        (fieldsData[key] && !fieldsData[key].value.length > 0) ||
+        (fieldsData[key] && Array.isArray(fieldsData[key].errors) && fieldsData[key].errors.length > 0)
+      ) {
+        notIsValid = true
+      }
+    }
+    setIsvalid(notIsValid)
+  }, [fieldsData])
+
+  const handleClick = () => {
+
+    eventEmitter({
+      email: email.value,
+      name: name.value,
+      last_name: lastName.value,
+      institution: institution.value,
+      institution_role: institutionAffiliation.value,
+      profession: profession.value,
+      birthday: new Date(dateBirth.value),
+      phone: phoneNumber.value, //phoneNumber.value.trim(),
+      phone_prefix: `+${phonePrefix}`,
+      password: password.value,
+      security_questions: [
+        {
+          question:securityQuestion1.value,
+          answer:securityAnswer1.value
+        },
+        {
+          question:securityQuestion2.value,
+          answer:securityAnswer2.value
+        }
+      ]
+    })
+  }
 
   return (
     <Paper className={classes.formBody}>
-      <Grid container xs={12} spacing={2} justify='center'>
-        <Fragment>
+      <Grid item container xs={12} justify="center" className={loading ? classes.loading : null}>
+        {loading && <LoaderComponent width="100p%" height={80} marginTop="20px" />}
+        {!loading && <Fragment>
           <TitleComponent
             justify='center'
             alignItems='center'
-            title='Update personal info'
+            title={t('registerPage.title')}
             variant='h5'
           />
-          <Grid container spacing={1} xs={12}>
-            <Grid item sm={12} md={6}>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
               <Input
                 disabled={false}
                 required
@@ -43,70 +105,243 @@ const UpdateProfileForm = () => {
                 variant="outlined"
                 margin="normal"
                 autoComplete="name"
-                {...email}/>
+                {...name}
+              />
             </Grid>
-            <Grid item sm={12} md={6}>
+            <Grid item xs={5}>
               <Input
                 disabled={false}
                 required
                 fullWidth
                 variant="outlined"
                 margin="normal"
-                autoComplete="name"
-                {...profession}
+                autoComplete="last name"
+                {...lastName}
               />
             </Grid>
           </Grid>
-          <Grid container direction='row' spacing={1} xs={12} alignItems='center'>
-            <Grid item sm={12} md={6}>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="email"
+                {...email}
+              />
+            </Grid>
+
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                margin="normal"
+                autoComplete="dateBirth"
+                {...dateBirth}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item xs={2}>
               <PhoneInput
-                inputStyle={{width:'90px'}}
-                country={'co'}
+                inputStyle={{ width:'90px' }}
+                country='co'
                 value={phonePrefix}
                 onChange={setPrefix}
               />
             </Grid>
-            <Grid item sm={12} md={6}>
-              {<PhoneNumber
-                xs={5}
-                phoneNumber={phoneNumber}
-              />}
-            </Grid>
+            {<PhoneNumber
+              xs={5}
+              phoneNumber={phoneNumber}
+            />}
           </Grid>
-          <Grid container spacing={1} xs={12}>
-            <Grid item sm={12} md={6}>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
               <Input
                 disabled={false}
                 required
                 fullWidth
                 variant="outlined"
                 margin="normal"
-                autoComplete="name"
-                {...institution}/>
+                autoComplete="institution"
+                {...institution}
+              />
             </Grid>
-            <Grid item sm={12} md={6}>
+
+            <Grid item xs={5}>
               <Input
                 disabled={false}
                 required
                 fullWidth
                 variant="outlined"
                 margin="normal"
-                autoComplete="name"
+                autoComplete="institutionAffiliation"
                 {...institutionAffiliation}
               />
             </Grid>
           </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={10}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="profession"
+                {...profession}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="security question 1"
+                {...securityQuestion1}
+              />
+            </Grid>
+
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="security answer 1"
+                {...securityAnswer1}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="security question 2"
+                {...securityQuestion2}
+              />
+            </Grid>
+
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="security answer 2"
+                {...securityAnswer2}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            spacing={1}
+            direction="row"
+            justify="center"
+          >
+            <Grid item xs={5}>
+              <Input
+                disabled={false}
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                autoComplete="password"
+                {...password}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <PasswordChecker
+                checkValue={password.value}
+                errorText={t('validators.validatePasswords')}
+                eventEmitter={(value) => {
+                  const { success } = value
+                  setVerificationPassword(success)
+                }}
+              />
+            </Grid>
+          </Grid>
+
           <Button
-            onClick={console.log(1)}
+            onClick={handleClick}
             variant="contained"
             color="primary"
+            disabled={!isValid && verificationPassword ? false : true}
           >
-            Save changes
+            Continue
           </Button>
-        </Fragment>
+        </Fragment>}
       </Grid>
     </Paper>
   )
-
 }
 export default UpdateProfileForm
