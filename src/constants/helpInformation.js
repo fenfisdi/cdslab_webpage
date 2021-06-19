3d68e0231e895232251c8cf09b8914ba46fc689a
Alejandro Campillo <alucardcampillo@gmail.com>
2021-06-19 10:40:48 -0500

    feat: compartmental help modal translated

--- src/constants/helpInformation.js
+++ src/constants/helpInformation.js
@@ -2,21 +2,18 @@ export const HELP_INFORMATION_NEW_SIMULATIONS = 'helpInformationCompartmental.ne
 
 export const HELP_INFORMATION_CHOOSE_SIMULATIONS = 'helpInformationCompartmental.chooseSimulation'
 
-export const HELP_INFORMATION_OPTIMIZE_PARAMETERS_SIMULATIONS = '...'
+export const HELP_INFORMATION_OPTIMIZE_PARAMETERS_SIMULATIONS = 'helpInformationCompartmental.optimizeParameters'
 
 export const HELP_INFORMATION_CONFIGURE_PARAMETERS_SIMULATIONS = 'helpInformationCompartmental.configureParameters'  
 
 export const HELP_INFORMATION_CONFIGURE_STATE_VARIANLES_SIMULATIONS = 'helpInformationCompartmental.stateVariables'
 
-export const HELP_INFORMATION_UPLOAD_DATA_SIMULATIONS = '...'
+export const HELP_INFORMATION_UPLOAD_DATA_SIMULATIONS = 'helpInformationCompartmental.uploadData'
 
 export const HELP_INFORMATION_CHOOSE_DATE_SIMULATIONS = 'helpInformationCompartmental.setDates'
 
 export const HELP_INFORMATION_REVIEW_CONFIGURATION_MESSAGE_SIMULATIONS = 'helpInformationCompartmental.simulationExecuted'
 
-
 export const HELP_INFORMATION_REVIEW_CONFIGURATION_INFORMATION_SIMULATIONS = 'helpInformationCompartmental.reviewConfiguration'
-
-
 
 export const HELP_INFORMATION_FIXED_PARAMETERS_SIMULATIONS = 'helpInformationCompartmental.fixedParameters'
