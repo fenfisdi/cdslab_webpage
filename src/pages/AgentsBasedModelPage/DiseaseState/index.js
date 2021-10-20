e575ee5124662ccd9875a74fc4ef591be1e5659a
jrodriguezcIntergrupo <jrodriguezc@intergrupo.com>
2021-10-20 11:36:44 -0500

    codeSmell: fix bug

--- src/pages/AgentsBasedModelPage/DiseaseState/index.js
+++ src/pages/AgentsBasedModelPage/DiseaseState/index.js
@@ -72,7 +72,7 @@ export default function TableTextInput({ data }) {
         <Row key={idRow}>
           {row.map((item, idItem) => {
             switch (item.type) {
-            case 'label' || 'label-valid' && (viewState || item.show):
+            case 'label' || ('label-valid' && (viewState || item.show)):
               return (
                 <Content key={idItem}>
                   <RowLeft key={idItem}>{item.content}</RowLeft>
